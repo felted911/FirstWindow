79e9bf0b964e6712f676719458aaf3c60aaa3aa2
Mark Grabanski <Mark Grabanski>
2016-10-24 21:40:37 -0400

    First commit

---
+++ Assets/Scripts/gui.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+
+
+public class gui: MonoBehaviour {
+
+
+	void OnGUI() {
+		GUI.Label(new Rect(10, 10, 100, 20), "Hello World!");
+	}
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+	}
+}
+
